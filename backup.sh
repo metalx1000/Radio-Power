c58efdf3de0fac14dc6658169ef3f7d25249180b
metalx1000 <metalx1000>
2017-10-31 11:26:46 -0400

    created install and backup script

---
+++ backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -vr /usr/local/bin* usr/local/bin/
+cp -vr /home/pi/wwwR/* home/pi/wwwR/
+cp -vr /etc/rc.local etc/
+
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#check if root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user"
+  echo "Trying to restart script as sudo"
+  sudo $0 $@
+  exit
+fi
+
+cp -vr home/pi/wwwR/* /home/pi/wwwR/
+cp -vr usr/local/bin/* /usr/local/bin/
+cp -vr etc/* /etc/
+
+chmod -R 777 /home/pi/wwwR
